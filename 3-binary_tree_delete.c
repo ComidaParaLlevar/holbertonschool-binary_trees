b490c8593aae018fa786c1d64a2d2d2e992c4da6
comidaparallevar <lyanosorio98@gmail.com>
2024-01-09 19:52:03 -0400

    added 4-binary_tree_is_leaf.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,17 +2,16 @@
 /**
  * binary_tree_delete - delete entire binary tree
  * @tree: pointer to root node of tree
- * 
+ *
  * Return: nothing
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
-if (tree == NULL) 
-    return;
+	if (tree == NULL)
+		return;
 
 binary_tree_delete(tree->left);
 binary_tree_delete(tree->right);
 
 free(tree);
-
 }
