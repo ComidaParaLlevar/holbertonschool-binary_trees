356f9dd82acb15d6895361dfa5ea5f143ceced31
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2024-01-11 01:23:56 -0400

    adding height and test folder - removed static

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,7 +5,7 @@
 * @tree: Pointer to the node to measures the height
 * Return: The height of the tree starting at @node
 */
-static size_t _height(const binary_tree_t *tree)
+size_t _height(const binary_tree_t *tree)
 {
 	size_t height_l;
 	size_t height_r;
