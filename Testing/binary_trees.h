6ceda9db635901257f93ea1e53c5ac2bb46b62fa
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2024-01-11 02:08:15 -0400

    adding 11. size

--- Testing/binary_trees.h
+++ Testing/binary_trees.h
@@ -38,5 +38,5 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 size_t binary_tree_depth(const binary_tree_t *tree);
-
+size_t binary_tree_size(const binary_tree_t *tree);
 #endif
