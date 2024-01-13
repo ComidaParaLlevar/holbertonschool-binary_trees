2b42844fb7b579fff03c830b8cfcec721e4a8986
Gabriel Velez <gabrielvelez@Gabriels-MacBook-Pro.local>
2024-01-12 23:07:39 -0400

    adding 16. is perfect

--- binary_trees.h
+++ binary_trees.h
@@ -46,8 +46,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
-int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 bool isPerfect(const binary_tree_t *tree, int expectedDepth);
-int find_depth_left(const binary_tree_t *tree);
 
 #endif
